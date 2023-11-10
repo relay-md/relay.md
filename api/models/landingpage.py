a2d37ebe4767040e286625fdae338883cb718c40
Fabian Schuh <mail@xeroc.org>
2023-11-10 14:26:54 +0100

    feat(alembic): upgrade script and new attribtues for landingpageemail

--- api/models/landingpage.py
+++ api/models/landingpage.py
@@ -14,7 +14,13 @@ class LandingPageEmail(Base):
     __tablename__ = "landing_emails"
 
     id: Mapped[uuid.UUID] = mapped_column(
-        primary_key=True, default=lambda x: uuid.uuid4(), nullable=False
+        primary_key=True, default=uuid.uuid4, nullable=False
     )
     email: str = Column(String(256))
-    confirm_date: datetime = Column(DateTime, default=datetime.utcnow)
+    created_date: datetime = Column(DateTime, default=datetime.utcnow)
+    confirm_token: Mapped[uuid.UUID] = mapped_column(default=uuid.uuid4)
+    confirm_date: datetime = Column(DateTime, nullable=True)
+
+    def confirm(self, token: str) -> None:
+        if self.confirm_token != uuid.UUID(token):
+            raise ValueError("Confirm code is invalid")
