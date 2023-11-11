7af1339a332412521e71490c57fe4297031894bf
Fabian Schuh <mail@xeroc.org>
2023-11-11 21:34:30 +0100

    fix: parse data from form

--- api/routes/v0.py
+++ api/routes/v0.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from fastapi import APIRouter, Depends
-from pydantic import BaseModel, EmailStr
+from fastapi import APIRouter, Depends, Form
+from pydantic import EmailStr
 
 from ..config import config
 from ..database import Session, get_session
@@ -10,24 +10,21 @@ from ..database import Session, get_session
 router = APIRouter(prefix="/v0")
 
 
-class EmailSubmitRequest(BaseModel):
-    email: EmailStr
-    first_name: str
-    last_name: str
-
-
 @router.post("/mail/submit")
 async def submitmail(
-    payload: EmailSubmitRequest, db: Session = Depends(get_session)
+    email: EmailStr = Form(),
+    first_name: str = Form(),
+    last_name: str = Form(),
+    db: Session = Depends(get_session),
 ) -> str:
     req = requests.post(
         f"https://{config.MAILCHIMP_API_SERVER}.api.mailchimp.com/3.0/lists/{config.MAILCHIMP_LIST_ID}/members",
         auth=("key", config.MAILCHIMP_API_KEY),
         headers={"content-type": "application/json"},
         json={
-            "email_address": payload.email,
+            "email_address": email,
             "status": "pending",
-            "merge_fields": {"FNAME": payload.first_name, "LNAME": payload.last_name},
+            "merge_fields": {"FNAME": first_name, "LNAME": last_name},
         },
     )
     req.raise_for_status()
