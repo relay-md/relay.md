cb898fad6b0e2995dba662dd16a6f69a5fb7ff7d
Fabian Schuh <mail@xeroc.org>
2023-11-14 09:03:08 +0100

    feat: distinct web module

--- backend/web.py
+++ backend/web.py
@@ -1,36 +1,19 @@
 # -*- coding: utf-8 -*-
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from .config import config
 from .database import Base, engine
-from .routes import api, home, login, v0
+from .routes import home, login
 
 # Create all tables
 Base.metadata.create_all(engine)
 
 app = FastAPI()
-app.add_middleware(SessionMiddleware, secret_key="!secret")
-app.include_router(v0.router)
+app.add_middleware(SessionMiddleware, secret_key=config.SECRET_KEY)
 
-assert home.router
-assert login.router
-assert api.router
-
-# app.include_router(home.router)
-# app.include_router(login.router)
-# app.include_router(api.router)
-#
-# Set all CORS enabled origins
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=config.API_ALLOWED_ORIGINS,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["Content-Range"],
-)
+app.include_router(home.router)
+app.include_router(login.router)
 
 # TODO:
 # exception handling:
