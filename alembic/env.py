4280954cf0ac3bc43193c1a0ae584bc556545dca
Fabian Schuh <mail@xeroc.org>
2023-11-17 13:25:12 +0100

    ops(alembic): import from correct module

--- alembic/env.py
+++ alembic/env.py
@@ -2,8 +2,8 @@
 from logging.config import fileConfig
 
 from alembic import context
-from api import models
-from api.database import Base, engine
+from backend import models
+from backend.database import Base, engine
 
 # need this so autoflake doesn't remove our model
 assert models
