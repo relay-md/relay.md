9d186f028f1e5eb1a78a0f05c03c2b0d34f9bc0e
Fabian Schuh <mail@xeroc.org>
2023-12-10 15:21:23 +0100

    feat: allow to use markdown in pug

--- backend/templates.py
+++ backend/templates.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import pypugjs
 from fastapi.templating import Jinja2Templates
+from markdown import markdown as markdown2html
 
 templates = Jinja2Templates(
     directory=r"backend/templates",
@@ -7,3 +9,8 @@ templates = Jinja2Templates(
 
 # Jade templating is nice for faster prototyping
 templates.env.add_extension("pypugjs.ext.jinja.PyPugJSExtension")
+
+
+@pypugjs.register_filter("markdown")
+def markdown(text, ast):
+    return markdown2html(text)
