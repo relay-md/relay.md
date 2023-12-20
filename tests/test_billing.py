712d1d14abaf747fc4802decd788c3a82e45130c
Fabian Schuh <mail@xeroc.org>
2023-12-20 11:47:24 +0100

    fix: unittests

--- tests/test_billing.py
+++ tests/test_billing.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime, timedelta
+from unittest.mock import patch
 from uuid import UUID
 
 import pytest
@@ -10,64 +11,94 @@ from backend.utils.dates import last_day_of_month
 
 
 @pytest.fixture
-def get_webhook_payload_recurring_contract():
-    def func(reference_id: UUID, shopper_id: UUID):
-        return {
-            "live": "false",
-            "notificationItems": [
-                {
-                    "NotificationRequestItem": {
-                        "additionalData": {
-                            "recurring.shopperReference": str(shopper_id),
-                            "recurring.recurringDetailReference": "M5N7TQ4TG5PFWR50",
-                            "hmacSignature": "YOUR_HMAC_SIGNATURE",
-                            "shopperReference": "YOUR_SHOPPER_REFERENCE",
-                            "checkoutSessionId": "******************",
-                        },
-                        "amount": {"currency": "US", "value": 0},
-                        "eventCode": "RECURRING_CONTRACT",
-                        "eventDate": "2023-06-20T16:09:48+02:00",
-                        "merchantAccountCode": "YOUR_MERCHANT_ACCOUNT",
-                        "merchantReference": str(reference_id),
-                        "originalReference": "DZ4DPSHB4WD2WN82",
-                        "paymentMethod": "mc",
-                        "pspReference": "M5N7TQ4TG5PFWR50",
-                        "reason": "",
-                        "success": "true",
-                    }
-                }
-            ],
-        }
-
-    return func
-
-
-@pytest.fixture
 def get_webhook_payload():
     def func(reference_id: UUID):
         return {
-            "live": "false",
-            "notificationItems": [
-                {
-                    "NotificationRequestItem": {
-                        "additionalData": {
-                            "authCode": "1234",
-                            "totalFraudScore": "10",
-                            "NAME2": "VALUE2",
-                            "NAME1": "VALUE1",
-                            "fraudCheck-6-ShopperIpUsage": "10",
+            "id": "evt_1OPMoVB5jLfQ2TsFkv9zLcGZ",
+            "object": "event",
+            "api_version": "2023-10-16",
+            "created": 1703067343,
+            "data": {
+                "object": {
+                    "id": "cs_test_a1ardhex9OzbE4h9DcBZzXxwiZhRAhsukrxN6Os3PHOYcsKquX58VpzHgO",
+                    "object": "checkout.session",
+                    "after_expiration": None,
+                    "allow_promotion_codes": None,
+                    "amount_subtotal": 3000,
+                    "amount_total": 3000,
+                    "automatic_tax": {"enabled": False, "status": None},
+                    "billing_address_collection": None,
+                    "cancel_url": "http://localhost:5000/payment/failed",
+                    "client_reference_id": str(reference_id),
+                    "client_secret": None,
+                    "consent": None,
+                    "consent_collection": None,
+                    "created": 1703067305,
+                    "currency": "eur",
+                    "currency_conversion": None,
+                    "custom_fields": [],
+                    "custom_text": {
+                        "after_submit": None,
+                        "shipping_address": None,
+                        "submit": None,
+                        "terms_of_service_acceptance": None,
+                    },
+                    "customer": "cus_PDoRlPaff7CDZO",
+                    "customer_creation": "always",
+                    "customer_details": {
+                        "address": {
+                            "city": None,
+                            "country": "DE",
+                            "line1": None,
+                            "line2": None,
+                            "postal_code": None,
+                            "state": None,
                         },
-                        "amount": {"currency": "EUR", "value": 10100},
-                        "eventCode": "AUTHORISATION",
-                        "eventDate": "2023-12-13T12:24:53+01:00",
-                        "merchantAccountCode": "ChainSquadGmbH",
-                        "merchantReference": str(reference_id),
-                        "pspReference": "ZCLWCV5MP8JSTC82",
-                        "reason": "1234:7777:12/2012",
-                        "success": "true",
-                    }
+                        "email": "fabian@chainsquad.com",
+                        "name": "Fabian Schuh",
+                        "phone": None,
+                        "tax_exempt": "none",
+                        "tax_ids": [],
+                    },
+                    "customer_email": "fabian@chainsquad.com",
+                    "expires_at": 1703153705,
+                    "invoice": "in_1OPMoSB5jLfQ2TsFGqEyEFbO",
+                    "invoice_creation": None,
+                    "livemode": False,
+                    "locale": None,
+                    "metadata": {},
+                    "mode": "subscription",
+                    "payment_intent": None,
+                    "payment_link": None,
+                    "payment_method_collection": "always",
+                    "payment_method_configuration_details": None,
+                    "payment_method_options": None,
+                    "payment_method_types": ["card"],
+                    "payment_status": "paid",
+                    "phone_number_collection": {"enabled": False},
+                    "recovered_from": None,
+                    "setup_intent": None,
+                    "shipping_address_collection": None,
+                    "shipping_cost": None,
+                    "shipping_details": None,
+                    "shipping_options": [],
+                    "status": "complete",
+                    "submit_type": None,
+                    "subscription": "sub_1OPMoSB5jLfQ2TsF5Yk9NCeY",
+                    "success_url": "http://localhost:5000/payment/success",
+                    "total_details": {
+                        "amount_discount": 0,
+                        "amount_shipping": 0,
+                        "amount_tax": 0,
+                    },
+                    "ui_mode": "hosted",
+                    "url": None,
                 }
-            ],
+            },
+            "livemode": False,
+            "pending_webhooks": 1,
+            "request": {"id": None, "idempotency_key": None},
+            "type": "checkout.session.completed",
         }
 
     return func
@@ -85,6 +116,7 @@ def test_create_invoice(
             price=3000,
             description="Foobar",
             team_id=team.id,
+            stripe_key="team-yearly",
         )
     ]
     person = models.PersonalInformation(
@@ -108,7 +140,16 @@ def test_create_invoice(
 
     webhook_payload = get_webhook_payload(invoice.id)
 
-    web_client.post("/payment/adyen/webhook", json=webhook_payload, auth=("foo", "bar"))
+    with patch("stripe.Webhook.construct_event", return_value=webhook_payload):
+        req = web_client.post(
+            "/payment/stripe/webhook",
+            json=webhook_payload,
+            auth=("foo", "bar"),
+            headers={"stripe-signature": "foobar"},
+        )
+
+    req.raise_for_status()
+    assert "error" not in req.json()
 
     dbsession.commit()
     dbsession.refresh(invoice)
@@ -118,52 +159,3 @@ def test_create_invoice(
     assert team.paid_until == last_day_of_month(
         datetime.utcnow() + timedelta(days=30 * 10)
     )
-
-
-def test_create_invoice_recurring(
-    dbsession, account, get_webhook_payload_recurring_contract, web_client, create_team
-):
-    team = create_team("private_team")
-    billing_repo = repos.InvoiceRepo(dbsession)
-    products = [
-        models.OrderItem(
-            name="Foobar",
-            quantity=10,
-            price=3000,
-            description="Foobar",
-            team_id=team.id,
-        )
-    ]
-    person = models.PersonalInformation(
-        name="Fabian Schuh",
-        email="fabian@chainsquad.com",
-        address_line1="Address 13, 24 Foobar",
-        city="Erlangen",
-        state="Bavaria",
-        zip="91058",
-        country_code="DE",
-        phone_country_code="+49",
-        phone_number="1706397354",
-    )
-    invoice = billing_repo.create(
-        models.Invoice(
-            user_id=account.id,
-            customer=person,
-            products=products,
-        )
-    )
-
-    webhook_payload = get_webhook_payload_recurring_contract(invoice.id, account.id)
-
-    web_client.post("/payment/adyen/webhook", json=webhook_payload, auth=("foo", "bar"))
-
-    dbsession.commit()
-    dbsession.refresh(invoice)
-
-    recurring_repo = repos.billing.RecurringPaymentTokenRepo(dbsession)
-    token = recurring_repo.get_by_kwargs(user_id=account.id)
-    assert token
-    assert token.user_id == account.id
-    assert token.recurringDetailReference == "M5N7TQ4TG5PFWR50"
-    assert token.originalReference == "DZ4DPSHB4WD2WN82"
-    assert token.pspReference == "M5N7TQ4TG5PFWR50"
