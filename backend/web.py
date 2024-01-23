91fa8b0e4e181db88029b43474a59fdf2818d0b3
Fabian Schuh <mail@xeroc.org>
2024-01-23 21:41:42 +0100

    chore(prerender): add prerenderReady state to signal site readiness

--- backend/web.py
+++ backend/web.py
@@ -99,8 +99,14 @@ Sitenmap: https://relay.md/sitemap.xml
 # after the `app` variable
 @app.middleware("http")
 async def prerender(request: Request, call_next):
-    user_agent = request.headers.get("user-agent")
-    if any([x in user_agent.lower() for x in get_config().PRERENDER_USER_AGENTS]):
-        return RedirectResponse(f"{get_config().PRERENDER_REDIRECT}{str(request.url)}")
+    user_agent = request.headers.get("user-agent", "").lower()
+    request.state.is_prerender = False
+    if "prerender" in user_agent:
+        request.state.is_prerender = True
+    else:
+        if any([x in user_agent for x in get_config().PRERENDER_USER_AGENTS]):
+            return RedirectResponse(
+                f"{get_config().PRERENDER_REDIRECT}{str(request.url)}"
+            )
     response = await call_next(request)
     return response
