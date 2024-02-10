74c228ac8f397acd54cdf3eee4b9d2999dce8108
Fabian Schuh <mail@xeroc.org>
2024-02-10 23:15:13 +0100

    chore: delete subscriptions when leaving team

--- backend/repos/user_team.py
+++ backend/repos/user_team.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from uuid import UUID
 
+from sqlalchemy import and_
+
+from ..models.team_topic import TeamTopic
 from ..models.user_team import UserTeam
+from ..models.user_team_topic import UserTeamTopic
 from .base import DatabaseAbstractRepository
 
 
@@ -12,4 +16,13 @@ class UserTeamRepo(DatabaseAbstractRepository):
         self.create_from_kwargs(user_id=user_id, team_id=team_id)
 
     def remove_member(self, membership):
+        # also delete all subscriptions to team topics
+        self._db.query(UserTeamTopic).filter(
+            and_(
+                UserTeamTopic.user_id == membership.user_id,
+                TeamTopic.team_id == membership.team_id,
+                UserTeamTopic.team_topic_id == TeamTopic.id,
+            )
+        ).delete(synchronize_session=False)
+
         self.delete(membership)
